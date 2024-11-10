ac42f0ddc19c40ca5a2a5df87fc59c7ccfcd1756
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-11-10 17:41:13 +0500

    feat: setup.py updated

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multi-language-translator',
+    version='0.0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    author='Usmonov Shoxruxmirzo',
+    author_email='usmonovshohruxmirzo@gmail.com',
+    description='Multi language translator',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/webbro-software/multi-language-translator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
