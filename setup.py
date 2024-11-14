e1e283bdeb01381d736f8e8efa37aeceed3135f9
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-11-14 10:29:01 +0500

    refactor: folder renamed to translator

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='multi-language-translator',
+    name='translator',
     version='0.0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
--- src/multi-language-translator/__init__.py
+++
@@ -1,3 +0,0 @@
-from translator import MultiLanguageTranslator
-
-__all__ = ['translator']
